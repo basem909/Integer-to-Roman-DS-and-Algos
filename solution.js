e427cb0188abc3769cb52d7b148cd802f42dc15c
basem909 <basemshams30@gmail.com>
2022-10-21 23:42:15 -0300

    The solution for the challenge

---
+++ solution.js
@@ -0,0 +1,33 @@
+const intToRoman =  (num) => {
+  const symbols = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let roman = "";
+  let multiplyer = 0;
+  for (let i = 0; i < symbols.length; i++) {
+    if (num % values[i] == 0) {
+      multiplyer = num / values[i];
+    } else {
+      multiplyer = Math.floor(num / values[i]);
+    }
+    multiplyer = symbols[i].repeat(multiplyer);
+    roman = roman + multiplyer;
+    num = num % values[i];
+  }
+  return roman;
+};
+
+console.log(intToRoman(3))
